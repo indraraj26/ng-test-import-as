3ffc0fc4e7da09877658851255e223b6a63b65f7
Indraraj <indraraj@infiniteit.biz>
2020-05-07 20:26:35 +0530

    feat: chalk, ts-node, prompt-confirm

---
+++ automate.ts
@@ -0,0 +1,24 @@
+import * as chalk from 'chalk';
+import * as env from './src/environments/environment';
+import * as envProduction from './src/environments/environment.prod';
+import * as envDev from './src/environments/environment.dev';
+
+let config: any = {};
+
+const log = console.log;
+// log(process.argv[2]);
+const arg = process.argv[2];
+
+if (arg == 'serve') {
+  config = { ...env.environment };
+} else if (arg == 'Production') {
+  config = { ...envProduction.environment };
+} else if (arg == 'Development') {
+  config = { ...envDev.environment };
+}
+
+log(chalk.red('================================================'));
+log(chalk.green('Configuration : ') + chalk.blue(arg));
+log(chalk.green('API URL : ') + chalk.blue(config.apiUrl));
+log(chalk.green('Storage URL : ') + chalk.blue(config.storageApiUrl));
+log(chalk.red('================================================'));
---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://test.com/api',
+  storageApiUrl: 'https://test.com/api/v1',
+};
